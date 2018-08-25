b8298038075de3a79f6562363d884633fd7b67c3
Bernard Hawke <bernardhawke@Bernards-MacBook-Pro.local>
2018-08-24 18:16:18 -0700

    Adding interactive mode to console program

--- Console/Console.swift
+++ Console/Console.swift
@@ -15,11 +15,26 @@ enum OutputType {
 
 class ConsoleIO {
 
+    /// This is how you get the input from the keyboard.
+    /// Apparently, the program hangs waiting for valid input when `keyboard.availableData` is called.
+    ///
+    /// - Returns: The input string.
+    func getInput() -> String {
+
+        let keyboard = FileHandle.standardInput
+        let inputData = keyboard.availableData
+        let inputString = String(bytes: inputData, encoding: .utf8)!
+
+        return inputString.trimmingCharacters(in: CharacterSet.newlines);
+    }
+
     func writeMessage(_ input: String, to: OutputType = .standard) {
         switch to {
         case .standard:
+            // Writes to standard output
             print(input)
         case .error:
+            // Writes to errors
             fputs("Error: \(input)", stderr)
         }
     }
--- Console/Panagram.swift
+++ Console/Panagram.swift
@@ -12,6 +12,7 @@ enum OptionType: String {
     case palindrome = "p"
     case anagram = "a"
     case help = "h"
+    case quit = "q"
     case unknown
 
     init(value: String) {
@@ -19,6 +20,7 @@ enum OptionType: String {
         case "a": self = .anagram
         case "p": self = .palindrome
         case "h": self = .help
+        case "q": self = .quit
         default: self = .unknown
         }
     }
@@ -27,13 +29,104 @@ enum OptionType: String {
 class Panagram {
     let consoleIO = ConsoleIO()
 
+    func interactiveMode() {
+        //1
+        consoleIO.writeMessage("Welcome to Panagram. This program checks if an input string is an anagram or palindrome.")
+        //2
+        var shouldQuit = false
+        while !shouldQuit {
+            //3
+            consoleIO.writeMessage("Type 'a' to check for anagrams or 'p' for palindromes type 'q' to quit.")
+            let (option, value) = getOption(consoleIO.getInput())
+
+            switch option {
+            case .anagram:
+                //4
+                consoleIO.writeMessage("Type the first string:")
+                let first = consoleIO.getInput()
+                consoleIO.writeMessage("Type the second string:")
+                let second = consoleIO.getInput()
+
+                //5
+                if StringProblems().areAnagrams(first, and: second) {
+                    consoleIO.writeMessage("\(second) is an anagram of \(first)")
+                } else {
+                    consoleIO.writeMessage("\(second) is not an anagram of \(first)")
+                }
+            case .palindrome:
+                consoleIO.writeMessage("Type a word or sentence:")
+                let s = consoleIO.getInput()
+                let isPalindrome = StringProblems().arePalindromes(s)
+                consoleIO.writeMessage("\(s) is \(isPalindrome ? "" : "not ")a palindrome")
+            case .quit:
+                shouldQuit = true
+            default:
+                //6
+                consoleIO.writeMessage("Unknown option \(value)", to: .error)
+            }
+        }
+    }
+
     func staticMode() {
         let argCount = CommandLine.argc
         let argument = CommandLine.arguments[1]
+
+        // This is for taking off the dash.
         let (option, value) = getOption(argument[1..<argument.count])
-        consoleIO.writeMessage("Argument count: \(argCount) Option: \(option) value: \(value)")
+
+        switch option {
+        case .anagram:
+            //2
+            if argCount != 4 {
+                if argCount > 4 {
+                    consoleIO.writeMessage("Too many arguments for option \(option.rawValue)", to: .error)
+                } else {
+                    consoleIO.writeMessage("Too few arguments for option \(option.rawValue)", to: .error)
+                }
+                consoleIO.printUsage()
+            } else {
+                //3
+                let first = CommandLine.arguments[2]
+                let second = CommandLine.arguments[3]
+
+                if StringProblems().areAnagrams(first, and: second) {
+                    consoleIO.writeMessage("\(second) is an anagram of \(first)")
+                } else {
+                    consoleIO.writeMessage("\(second) is not an anagram of \(first)")
+                }
+            }
+        case .palindrome:
+            //4
+            if argCount != 3 {
+                if argCount > 3 {
+                    consoleIO.writeMessage("Too many arguments for option \(option.rawValue)", to: .error)
+                } else {
+                    consoleIO.writeMessage("Too few arguments for option \(option.rawValue)", to: .error)
+                }
+                consoleIO.printUsage()
+            } else {
+                //5
+                let s = CommandLine.arguments[2]
+                let isPalindrome = StringProblems().arePalindromes(s)
+                consoleIO.writeMessage("\(s) is \(isPalindrome ? "" : "not ")a palindrome")
+            }
+        //6
+        case .help:
+            consoleIO.printUsage()
+        case .quit:
+            break
+        case .unknown:
+            //7
+            consoleIO.writeMessage("Unknown option \(value)")
+            consoleIO.printUsage()
+        }
+
     }
 
+    /// Wraps the option string inside an enum
+    ///
+    /// - Parameter option: the option string
+    /// - Returns: the returned enum
     func getOption(_ option: String) -> (type: OptionType, value: String) {
         return (type: OptionType(value: option), value: option)
     }
